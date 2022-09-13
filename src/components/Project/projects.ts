49b955afcc870c217a507de820d2525827d5fe9a
Anton Lovrić <antonlovri81@gmail.com>
2022-09-13 20:08:33 +0200

    converted project images to webp

--- src/components/Project/projects.ts
+++ src/components/Project/projects.ts
@@ -12,16 +12,16 @@ export const projects: IProject[] = [
         title: 'Free Bird Music',
         description:
             'This is a fullstack application created as part of my Bachelor’s Thesis. The backend is created using Laravel while the frontend is created using Nuxt 3.',
-        image: 'assets/freebird.png',
+        image: 'assets/freebird.webp',
         frontend: 'https://github.com/antonlovric/freebird-front',
         backend: 'https://github.com/antonlovric/freebird-music',
         demo: 'https://freebird-music.vercel.app/',
     },
     {
-        title: 'Alojzije Janković',
+        title: 'Alojzije Jankovic',
         description:
-            'This is a frontend project made using vanilla HTML, SCSS and JS. It is a personal website made for chess grandmaster Alojzije Janković. It includes general info, chess game overview and a phrase generator',
-        image: 'assets/jankovic.png',
+            'This is a frontend project made using vanilla HTML, SCSS and JS. It is a personal website made for chess grandmaster Alojzije Jankovic. It includes general info, chess game overview and a phrase generator',
+        image: 'assets/jankovic.webp',
         frontend: 'https://github.com/antonlovric/AlojzijeJankovic',
         demo: 'https://antonlovric.github.io/AlojzijeJankovic/',
     },
@@ -29,7 +29,7 @@ export const projects: IProject[] = [
         title: 'BMI Calculator',
         description:
             'Simple BMI calculator application made using vanilla HTML, SCSS and JS and ProgressBar js for progress bar animation. ',
-        image: 'assets/bmi.png',
+        image: 'assets/bmi.webp',
         frontend: 'https://github.com/antonlovric/BMICalculator',
         demo: 'https://antonlovric.github.io/BMICalculator/',
     },
@@ -37,7 +37,7 @@ export const projects: IProject[] = [
         title: 'Portfolio',
         description:
             'This portfolio was made using Astro since it offers good, lightweight SSG capabilities. Also used React to create componentes needed for this project and styled them using SCSS.',
-        image: 'assets/portfolio.png',
+        image: 'assets/portfolio.webp',
         frontend: 'https://github.com/antonlovric/Portfolio',
     },
 ];
