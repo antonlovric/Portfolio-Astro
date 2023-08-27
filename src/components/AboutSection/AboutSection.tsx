e27698a4d445bd6b4a3ea82f95a734fa52e96bbe
Anton Lovrić <antonlovri81@gmail.com>
2023-08-27 14:26:50 +0200

    refactor: updated bio

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -8,18 +8,18 @@ const AboutSection = () => {
       <div className='container'>
         <div className='text-container'>
           <p>
-            I was born on May 1st 1999. and have always felt a strong passion towards computers.
+            I was born on May 1st 1999 and have always felt a strong passion towards computers.
             After graduating high school as a mechanical engineer and participating in various
             competitions in mechanics and mechanical constructions, I decided to enroll at Faculty
             of Organization and Informatics in Varazdin. I earned my Bachelor’s degree in September
-            2022. and I am currently working on my master's degree.
+            2022 and I am currently working on my master's degree.
           </p>
           <p>
             I have been working on my development skills for years, seriously focusing on frontend
             development in 2020. Since then I have worked in various environments with different
-            tools. I also have 7 months of professional experience in frontend development where I
-            primarily used Next.js, React and React Native. Other than frontend development, I'm
-            also working on my backend skills primarily using Laravel.
+            tools. I also have a year and a half of professional experience in frontend development
+            where I primarily used Next.js, React and React Native. Other than frontend development,
+            I'm also working on my backend skills primarily using Laravel and Node.
           </p>
         </div>
         <img src='/assets/profilna.webp' alt='' />
